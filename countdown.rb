408677e56224b86a84f3aa616479003fac3b346a
Allan <kipropsallan@gmail.com>
2022-08-21 17:00:03 +0300

    Done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
+midnight = 5
+def countdown (midnight = 10)
+  while midnight > 0
+    puts "#{midnight} SECOND(S)!"
+    midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep (midnight = 10)
+  while midnight >= 0
+    puts "#{midnight} SECOND(S)!"
+    midnight -= 1
+    sleep 1
+  end
+  "HAPPY NEW YEAR!"
 end
